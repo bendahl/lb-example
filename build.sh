5b6c694dbf0a6cbb7e0fe04245fb82675a8feaae
Benjamin Dahlmanns <bendahl@users.noreply.github.com>
2020-01-14 10:50:07 +0100

    Add more information to status message in order to make things easier during demos and testing

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Building go binary 'lb-example'"
 go build -tags netgo -a -v
 
 echo "Building Docker image"
-docker build -t lb-example:1.0 .
+docker build -t lb-example:2.0 .
 
 echo "------------------------------------------------------------------------"
 echo "DONE. You may now setup the local test cluster using 'docker-compose up'"
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	uuid "github.com/google/uuid"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
 	id := uuid.New()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { _, _ = w.Write([]byte(id.String())) })
+	hostname, _ := os.Hostname()
+	starttime := fmt.Sprint(time.Now().Format(time.RFC850))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Hello from host '%s' started on %s. UUID: %s", hostname, starttime, id)
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
