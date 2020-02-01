56e658e9d2ba5f1db49815bbb280096b49f4ab8e
Benjamin Dahlmanns <bendahl@users.noreply.github.com>
2020-02-01 12:07:08 +0100

    Add option to set port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	uuid "github.com/google/uuid"
 	"log"
@@ -10,11 +11,14 @@ import (
 )
 
 func main() {
+	port := flag.Int("p", 8080, "port to listen on")
+	flag.Parse()
+
 	id := uuid.New()
 	hostname, _ := os.Hostname()
 	starttime := fmt.Sprint(time.Now().Format(time.RFC850))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "Hello from host '%s' started on %s. UUID: %s", hostname, starttime, id)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", *port), nil))
 }
